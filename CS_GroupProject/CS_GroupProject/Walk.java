15cc7665b5f46c81138966f3af14424f376a85de
Jack <ityoung98@gmail.com>
2016-04-12 13:39:14 -0400

    Added files via upload

---
+++ CS_GroupProject/CS_GroupProject/Walk.java
@@ -0,0 +1,12 @@
+public class Walk extends Animals{
+  
+  public enum 
+  
+  private int leg;
+  private int size;
+  
+  public Walk(Gender gender,boolean able, int age, String name, int leg, int size){
+   super(gender, able, age, name);
+   this.leg=leg;
+   this.size=size;}
+}
