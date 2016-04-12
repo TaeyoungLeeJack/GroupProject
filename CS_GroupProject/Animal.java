e665275f24adb5cdca73354ea2435b974703da54
Jack <ityoung98@gmail.com>
2016-04-12 13:40:58 -0400

    Added files via upload

--- CS_GroupProject/Animal.java
+++ CS_GroupProject/Animal.java
@@ -1,2 +1,23 @@
 public class Animals{
+  enum Gender {Male,Female};
+  
+  private Gender gender;
+  private boolean able;
+  private int age;
+  private String name;
+  
+  public Animals{Gender gender, boolean able, int age, String name){
+  this.gender=gender;
+  this.able= able;
+  this.age=age;
+  this.name=name}
+  
+  public Gender getGender(){return this.gender;}
+  public int getAge(){return this.age;}
+  public boolean getAble(){return this.able;}
+  public String getName(){return this.name;}
+  
+  public void intro(){
+   System.out.println("Hlello. My name is " + this.getName())}
+  
 }
---
+++ CS_GroupProject/Bird.java
@@ -0,0 +1,18 @@
+public class Bird extends Animals{
+  
+  
+  private int wing;
+  private int size;
+  
+  public Bird(Gender gender,boolean able, int age, String name, int wing,int size){
+    super(gender, able, age, name);
+    this.wing=wing;
+    this.size=size;}
+    
+  public int getWing(){return this.wing;}
+  public int getSize(){return this.size;}
+
+
+public void intro(){
+System.out.println("Hello. My name is "+ this.getName() + ". I believe I can fly!"); }
+}
